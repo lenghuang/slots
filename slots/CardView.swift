4373b4e6bb018aebaddcdc04fe9b8fef3d8bcf99
lenghuang <lendevelops@gmail.com>
2020-08-03 18:52:35 -0400

    background card functionality

---
+++ slots/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  slots
+//
+//  Created by Len Huang on 8/3/20.
+//  Copyright © 2020 Len Huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
