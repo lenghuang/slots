2f1442eb3face3fc85d34ce2f1b1595fb595ab14
lenghuang <lendevelops@gmail.com>
2020-08-03 14:53:28 -0400

    Initial Commit

---
+++ slots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slots
+//
+//  Created by Len Huang on 8/3/20.
+//  Copyright © 2020 Len Huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
